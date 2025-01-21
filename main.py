69b6b4b4493b2db8944dc459a182b6289a7c4c9d
Jinan Yousuf <jinanyousuf@gmail.com>
2025-01-21 16:24:11 -0500

    import

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 
